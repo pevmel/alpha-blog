729e58ab811a8febba5bea5fd39fca17503aaf4b
Viacheslav Panchuk <pevmel@gmail.com>
2018-01-01 20:05:16 +0200

    Creat show action and add gravatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles_path
+  has_many :articles
   before_save {self.email=email.downcase}
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {in: 3..30}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
