09c0dd8738115104f15fbbfac90b50833d338fd6
Viacheslav Panchuk <pevmel@gmail.com>
2018-01-05 17:27:28 +0200

    Fix tests and update navbar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "Jone", email: "jone@dsf.com", password: "pasdf", admin: true)
   end
 
   test "should get categories index page" do
@@ -11,7 +12,8 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get new page" do
+  test "should get new page" do    
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -19,6 +21,13 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show page" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "sports2"}
+    end
+    assert_redirected_to categories_path
   end
 
 
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: "Jone", email: "jone@dsf.com", password: "pasdf", admin: true)
+  end
+
   test "new category form and create category" do
+    sing_in_as(@user, @user.password)
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -13,6 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sing_in_as(@user, @user.password)
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
