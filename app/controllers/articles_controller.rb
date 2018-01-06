ebcb1371e6b8067a4bfe7afceccd9a6341c220e4
Viacheslav Panchuk <pevmel@gmail.com>
2018-01-06 15:56:22 +0200

    Edit category feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,8 +37,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
-    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+
   end
 
   def destroy
